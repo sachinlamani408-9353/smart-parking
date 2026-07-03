74c23a51953116b4362782c373e12d3161d28f15
sachin lamani <sachinlamani408@gmail.com>
2026-07-03 18:45:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+const sheetID =
+"1XG93Hf0LbT3ka6ljcGnUqLxjvCnVpeGBoOb6traRzck";
+
+
+const url =
+`https://opensheet.elk.sh/${sheetID}/Sheet1`;
+
+
+async function getData()
+{
+    try
+    {
+
+        let response = await fetch(url);
+
+        let data = await response.json();
+
+        console.log(data);
+
+
+        document.getElementById("total").innerText =
+        data[0]["totalslots"];
+
+
+        document.getElementById("occupied").innerText =
+        data[0]["occupied slots"];
+
+
+        document.getElementById("available").innerText =
+        data[0]["available slots"];
+
+
+
+        let available =
+        Number(data[0]["available slots"]);
+
+
+        if(available > 0)
+        {
+            document.getElementById("status").innerHTML =
+            "🟢 PARKING AVAILABLE";
+        }
+        else
+        {
+            document.getElementById("status").innerHTML =
+            "🔴 PARKING FULL";
+        }
+
+    }
+
+    catch(error)
+    {
+        console.log(error);
+    }
+
+}
+
+
+getData();
+
+
+setInterval(getData,3000);
